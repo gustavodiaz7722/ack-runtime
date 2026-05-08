ad4740b496730342fcb00e879c5ff63745be9105
Gustavo Diaz <gustidia@amazon.com>
2026-05-08 21:22:27 +0000

    Create compare zero functions

---
+++ pkg/compare/zero.go
@@ -0,0 +1,29 @@
+package compare
+
+import (
+	"reflect"
+)
+
+func IsZeroValue(i interface{}) bool {
+	if i == nil {
+		return false
+	}
+	v := reflect.ValueOf(i)
+	// Dereference pointer to check if the pointed-to value is zero
+	if v.Kind() == reflect.Ptr {
+		if v.IsNil() {
+			return false
+		}
+		return v.Elem().IsZero()
+	}
+	return v.IsZero()
+}
+
+// IsNilEqualsZero returns true if 'a' satisfies IsNil and 'b' satisfies IsZeroValue.
+func IsNilEqualsZero(a interface{}, b interface{}) bool {
+	return IsNil(a) && IsZeroValue(b)
+}
+
+func IsZeroEqualsNil(a interface{}, b interface{}) bool {
+	return IsNil(a) && IsZeroValue(b)
+}
